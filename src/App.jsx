8a526112453485a53aeae007a2ada619f45d1789
p2arthur <arthurrabelopro@gmail.com>
2022-12-12 17:27:55 -0300

    Created the Boiler plate for the Component React App

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>I'm Button 1</Button>
+      </div>
+      <div>
+        <Button>I'm Button 2</Button>
+      </div>
+      <div>
+        <Button>I'm Button 3</Button>
+      </div>
+      <div>
+        <Button>I'm Button 4</Button>
+      </div>
+      <div>
+        <Button>I'm Button 5</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,7 @@
+//Wrapper Element
+function Button({ children }) {
+  //Underlying Element/Wrapped element
+  return <button>{children}</button>;
+}
+
+export default Button;
