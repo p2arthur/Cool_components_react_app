67c02e133440a853f9d10e90f05d5c58112cf397
p2arthur <arthurrabelopro@gmail.com>
2022-12-13 09:59:02 -0300

    Added all variations of button and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,179 @@
 import Button from "./Button";
+import {
+  MdAccountBalanceWallet,
+  MdBookmarkAdd,
+  MdFitScreen,
+} from "react-icons/md";
 
 function App() {
   return (
     <div>
-      <Button primary>Primary</Button>
+      <div className="grid grid-cols-6">
+        <Button inactive>Inactive</Button>
 
-      <Button secondary>Secondary</Button>
+        <Button primary>Primary</Button>
 
-      <Button success>Success</Button>
+        <Button secondary>Secondary</Button>
 
-      <Button warning>Warning</Button>
+        <Button success>Success</Button>
 
-      <Button danger>Danger</Button>
+        <Button warning>Warning</Button>
 
-      <div>
+        <Button danger>Danger</Button>
+      </div>
+
+      <div className="grid grid-cols-6">
+        <Button inactive>
+          Inactive <MdAccountBalanceWallet />
+        </Button>
+
+        <Button primary>
+          Primary <MdAccountBalanceWallet />
+        </Button>
+
+        <Button secondary>
+          Secondary <MdAccountBalanceWallet />
+        </Button>
+
+        <Button success>
+          Success <MdAccountBalanceWallet />
+        </Button>
+
+        <Button warning>
+          Warning <MdAccountBalanceWallet />
+        </Button>
+
+        <Button danger>
+          Danger <MdAccountBalanceWallet />
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-6">
+        <Button inactive outline>
+          Inactive
+        </Button>
+
+        <Button primary outline>
+          Primary
+        </Button>
+
+        <Button secondary outline>
+          Secondary
+        </Button>
+
+        <Button success outline>
+          Success
+        </Button>
+
+        <Button warning outline>
+          Warning
+        </Button>
+
+        <Button danger outline>
+          Danger
+        </Button>
+      </div>
+      <div className="grid grid-cols-6">
+        <Button inactive rounded>
+          Inactive
+        </Button>
+
         <Button primary rounded>
-          Primary Rounded
+          Primary
         </Button>
 
         <Button secondary rounded>
-          Secondary Rounded
+          Secondary
         </Button>
 
         <Button success rounded>
-          Success Rounded
+          Success
         </Button>
 
         <Button warning rounded>
-          Warning Rounded
+          Warning
         </Button>
 
         <Button danger rounded>
-          Danger Rounded
+          Danger
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-6">
+        <Button inactive rounded>
+          Inactive <MdAccountBalanceWallet />
+        </Button>
+
+        <Button primary rounded>
+          Primary <MdBookmarkAdd />
+        </Button>
+
+        <Button secondary rounded>
+          Secondary <MdFitScreen />
+        </Button>
+
+        <Button success rounded>
+          Success <MdAccountBalanceWallet />
+        </Button>
+
+        <Button warning rounded>
+          Warning <MdFitScreen />
+        </Button>
+
+        <Button danger outline rounded>
+          Danger <MdBookmarkAdd />
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-6">
+        <Button inactive rounded outline>
+          Inactive
+        </Button>
+
+        <Button primary rounded outline>
+          Primary
+        </Button>
+
+        <Button secondary rounded outline>
+          Secondary
+        </Button>
+
+        <Button success rounded outline>
+          Success
+        </Button>
+
+        <Button warning rounded outline>
+          Warning
+        </Button>
+
+        <Button danger rounded outline>
+          Danger
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-6">
+        <Button inactive rounded outline>
+          Inactive <MdAccountBalanceWallet />
+        </Button>
+
+        <Button primary rounded outline>
+          Primary <MdAccountBalanceWallet />
+        </Button>
+
+        <Button secondary rounded outline>
+          Secondary <MdAccountBalanceWallet />
+        </Button>
+
+        <Button success rounded outline>
+          Success <MdAccountBalanceWallet />
+        </Button>
+
+        <Button warning rounded outline>
+          Warning <MdAccountBalanceWallet />
+        </Button>
+
+        <Button danger rounded outline>
+          Danger <MdAccountBalanceWallet />
         </Button>
       </div>
     </div>
--- src/Button.jsx
+++ src/Button.jsx
@@ -4,6 +4,7 @@ import className from "classnames";
 //Wrapper Element
 function Button({
   children,
+  inactive,
   primary,
   secondary,
   success,
@@ -12,22 +13,28 @@ function Button({
   outline,
   rounded,
 }) {
-  //Setting the className using tailwind to each one of the props
-  const finalClassName = className({
-    "bg-blue-600": primary,
-    "bg-black": secondary,
-    "bg-green-500": success,
-    "bg-red-500": danger,
-    "bg-yellow-400": warning,
-    "rounded-3xl": rounded,
-  });
+  //Setting the className conditionally using tailwind to each one of the props
+  const theClass = className(
+    "flex items-center justify-center m-3 p-3 cursor-pointer ",
+    {
+      "bg-white border-2 border-gray-900": inactive,
+      "border-blue-500 bg-blue-500 border-2  text-white": primary,
+      "bg-gray-900 border-2 border-gray-900 text-white": secondary,
+      "bg-green-500 border-2 border-green-500 text-white": success,
+      "bg-red-500 border-2 border-red-500 text-white": danger,
+      "bg-yellow-300 border-2 border-yellow-300": warning,
+      "rounded-full": rounded,
+      "button-outlined": outline,
+      "text-gray-900 ": outline && secondary,
+      "text-blue-500 ": outline && primary,
+      "text-green-500": outline && success,
+      "text-red-500": outline && danger,
+      "text-yellow-300": outline && warning,
+    }
+  );
 
   //Underlying Element/Wrapped element
-  return (
-    <button className={`${finalClassName} text-white m-3 p-3 cursor-pointer`}>
-      {children}
-    </button>
-  );
+  return <button className={`${theClass}`}>{children}</button>;
 }
 
 //Checking if there are conflicting variation props using prop-types library
