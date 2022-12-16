e1e9a1aee583c681224a65767a32a379a45f6ccd
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-16 16:55:10 -0300

    Added a new component called sidebar to hold the link components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import Link from "./Link";
+
+function Sidebar() {
+  const links = [
+    { label: "Dropdown", path: "/" },
+    { label: "Accordion", path: "/accordion" },
+    { label: "Buttons", path: "/buttons" },
+  ];
+
+  const renderedItems = links.map((link) => (
+    <Link
+      activeClassName="border-l-2 border-blue-500 font-bold pl-2"
+      className="mb-3"
+      key={link.label}
+      to={link.path}
+    >
+      {link.label}
+    </Link>
+  ));
+
+  return (
+    <div className="flex flex-col items-start sticky top-0 overflow-y-scroll">
+      {renderedItems}
+    </div>
+  );
+}
+
+export default Sidebar;
