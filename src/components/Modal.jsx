0d1dc6d3d66a82d53c49bdf88d879146e058f7c4
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-17 20:20:36 -0300

    Finish Modal component and its functionality

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,32 @@
-function Modal() {
-  return <div>Modal</div>;
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+function Modal({ onClose, children, actionBar }) {
+  const handleCloseClick = () => onClose();
+
+  useEffect(() => {
+    const body = document.body;
+    body.classList.add("overflow-hidden");
+
+    return () => body.classList.remove("overflow-hidden");
+  }, []);
+
+  //Creating a portal to .modal-container for our modal so it can be reutilized without caring if there is a positioned parent component
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={handleCloseClick}
+        className="absolute inset-0 bg-black opacity-80"
+      ></div>
+      <div className="absolute inset-40 p-10 bg-white">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
+      </div>
+    </div>,
+    document.querySelector(".modal-container")
+  );
 }
 
 export default Modal;
--- src/pages/ModalPage.jsx
+++ src/pages/ModalPage.jsx
@@ -1,7 +1,130 @@
 import Modal from "../components/Modal";
+import Button from "../components/Button";
+import { useState } from "react";
 
 function ModalPage() {
-  return <Modal></Modal>;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => setShowModal(false);
+
+  const actionBar = (
+    <div>
+      <Button onClick={handleClose} success>
+        I accept
+      </Button>
+    </div>
+  );
+
+  const modalElement = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p className="font-semibold">
+        Here's an important agreement for you to accept
+      </p>
+    </Modal>
+  );
+
+  return (
+    <div>
+      <Button onClick={handleClick} primary>
+        Open modal
+      </Button>
+      {showModal ? modalElement : null}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+        molestiae tempore ipsa? Enim unde pariatur, reiciendis dolores nulla cum
+        aliquam dolore. Facere, accusamus autem nemo eveniet nihil eos
+        voluptatum facilis.
+      </p>
+    </div>
+  );
 }
 
 export default ModalPage;
