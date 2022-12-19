d77e8c2ad6a52ccce88d97d5916a3c735510f4de
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-19 10:44:09 -0300

    Finish fully reusable basic table component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,33 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ config, data, keyFnHelper }) {
+  //Mapping through the config prop to dinamically render the label into the table headers
+  const renderedHeaders = config.map((column) => (
+    <th className="text-slate-900" key={column.label}>
+      {column.label}
+    </th>
+  ));
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column, index) => (
+      <td className="p-2 m-1 text-slate-900" key={index}>
+        {column.render(rowData)}
+      </td>
+    ));
+
+    return (
+      <tr className="border-b" key={keyFnHelper(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2 rounded w-full">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,7 +1,33 @@
 import Table from "../components/Table";
 
 function TablePage() {
-  return <Table />;
+  //Create an array of objects to be displayed inside the table component
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-300", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
+  ];
+
+  //Create a config array to dinamically pass properties to the table component
+  const config = [
+    { label: "Fruits", render: (fruit) => fruit.name },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
+    },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
+
+  const keyFnHelper = (fruit) => {
+    return fruit.name;
+  };
+
+  return (
+    <div>
+      <Table keyFnHelper={keyFnHelper} config={config} data={data} />
+    </div>
+  );
 }
 
 export default TablePage;
