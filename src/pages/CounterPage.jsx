2b957680a6026a62fec90ccd89c54da32ef605d8
Arthur Rabelo <arthurrabelopro@gmail.com>
2022-12-23 12:12:08 -0300

    refactor CounterComponent to useReducer

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,30 +1,44 @@
 import Button from "../components/Button";
-import { useState } from "react";
+import { useReducer } from "react";
 import Panel from "../components/Panel";
 
-function CounterPage({ initialCount }) {
-  const [counter, setCounter] = useState(initialCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+const reducer = (state, action) => {};
 
-  const incrementCounter = () => setCounter(counter + 1);
-  const decrementCounter = () => setCounter(counter - 1);
+function CounterPage({ initialCount }) {
+  //const [counter, setCounter] = useState(initialCount);
+  //const [valueToAdd, setValueToAdd] = useState(0);
+
+  //Using reducer instead of useState
+  const [state, dispatch] = useReducer(reducer, {
+    counter: initialCount,
+    valueToAdd: 0,
+  });
+
+  console.log(state);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
 
-    setValueToAdd(value);
+    //setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCounter(counter + valueToAdd);
-    setValueToAdd(0);
+    //setCounter(counter + valueToAdd);
+    //setValueToAdd(0)
   };
 
   return (
     <Panel className="my-3">
       <div className="flex flex-col items-center">
-        <h2 className="text-3xl">Count is: {counter}</h2>
+        <h2 className="text-3xl">Count is: {state.counter}</h2>
         <div className="flex">
           <Button onClick={decrementCounter} danger>
             Decrease counter -
@@ -41,7 +55,7 @@ function CounterPage({ initialCount }) {
         >
           <input
             onChange={handleChange}
-            value={valueToAdd || ""}
+            value={state.valueToAdd || ""}
             className="border p-1 m-3"
             type="number"
           />
